ddfa4b59fa7b39d7d7127f581c38f55b55d40009
Ryan Wilson <inbox@ryanwilson.space>
2024-11-14 13:31:04 -0500

    Support multiple messages in the logger

--- src/AeroSandbox/src/shared/Loggers.ts
+++ src/AeroSandbox/src/shared/Loggers.ts
@@ -3,17 +3,6 @@
  * This module contains the loggers for aero and AeroSandbox
 */
 
-import type { Config } from "$aero/types/config";
-
-// @ts-ignore
-declare const self: WorkerGlobalScope &
-    typeof globalThis & {
-        config: Config;
-    };
-
-// Webpack Feature Flags
-// biome-ignore lint/style/useSingleVarDeclarator: <explanation>
-let DEBUG: boolean;
 
 type htmlTemplatingCallbackType = (errStr: string) => string;
 
@@ -22,123 +11,143 @@ const fatalErrBubbleStyle = genBubbleStyle("#db3631");
 
 // TODO: Support optionalSecondaryBubble after the branding and in the color green :)
 class GenericLogger {
-    log(branding: string, msg: string, optionalSeconaryBubble?: string): void {
-        console.log(`%c${branding}%c ${msg}`, `${aeroBubbleStyle}`, "");
-    }
-    warn(branding: string, msg: string, optionalSeconaryBubble?: string): void {
-        console.warn(`%c${branding}%c ${msg}`, `${aeroBubbleStyle}`, "");
-    }
-    debug(
-        branding: string,
-        msg: string,
-        optionalSecondaryBubble?: string
-    ): void {
-        if (DEBUG) {
-            console.debug(`%c${branding}%c ${msg}`, `${aeroBubbleStyle}`, "");
-        }
-    }
-    error(
-        branding: string,
-        msg: string,
-        optionalSeconaryBubble?: string
-    ): void {
-        console.error(`%c${branding}%c ${msg}`, `${aeroBubbleStyle}`, "");
-    }
-    fatalErr(branding: string, msg: string): void {
-        console.error(
-            `%c${branding}%c %cfatal%c ${msg}`,
-            `${aeroBubbleStyle}`,
-            "",
-            `${fatalErrBubbleStyle}`,
-            ""
-        );
-    }
+	log(branding: string, msgs: string[] | string, optionalSeconaryBubble?: string): void {
+		if (!Array.isArray(msgs))
+			msgs = [msgs];
+
+		for (const msg in msgs)
+			console.log(`%c${branding}%c ${msg}`, `${aeroBubbleStyle}`, "");
+	}
+	warn(branding: string, msgs: string[] | string, optionalSeconaryBubble?: string): void {
+		if (!Array.isArray(msgs))
+			msgs = [msgs];
+
+		for (const msg in msgs)
+			console.warn(`%c${branding}%c ${msg}`, `${aeroBubbleStyle}`, "");
+	}
+	debug(
+		branding: string,
+		msgs: string[] | string,
+		optionalSecondaryBubble?: string
+	): void {
+		if (DEBUG) {
+			if (!Array.isArray(msgs))
+				msgs = [msgs];
+
+			for (const msg in msgs)
+				console.debug(`%c${branding}%c ${msg}`, `${aeroBubbleStyle}`, "");
+		}
+	}
+	error(
+		branding: string,
+		msgs: string[] | string,
+		optionalSeconaryBubble?: string
+	): void {
+		if (!Array.isArray(msgs))
+			msgs = [msgs];
+
+		for (const msg in msgs)
+			console.error(`%c${branding}%c ${msg}`, `${aeroBubbleStyle}`, "");
+	}
+	fatalErr(branding: string, msgs: string[] | string): void {
+		if (!Array.isArray(msgs))
+			msgs = [msgs];
+
+		for (const msg in msgs)
+			console.error(
+				`%c${branding}%c %cfatal%c ${msg}`,
+				`${aeroBubbleStyle}`,
+				"",
+				`${fatalErrBubbleStyle}`,
+				""
+			);
+	}
 }
 
 interface LoggerOptions {
-    htmlTemplatingCallback?: htmlTemplatingCallbackType;
+	htmlTemplatingCallback?: htmlTemplatingCallbackType;
 }
 
 class AeroLogger extends GenericLogger {
-    options: LoggerOptions;
-    debugMode: boolean;
+	options: LoggerOptions;
+	debugMode: boolean;
 
-    // TODO: I switched around the args replace every construct of Aerologger
-    constructor(debugMode: boolean, options?: LoggerOptions) {
-        this.debugMode = debugMode;
-        super();
-        if (options) this.options = options;
-    }
+	// TODO: I switched around the args replace every construct of Aerologger
+	constructor(debugMode: boolean, options?: LoggerOptions) {
+		this.debugMode = debugMode;
+		super();
+		if (options) this.options = options;
+	}
 
-    log(msg: string): void {
-        super.log("aero SW", msg);
-    }
-    warn(msg: string): void {
-        super.warn("aero SW", msg);
-    }
-    debug(msg: string): void {
-        super.warn("aero SW", msg);
-    }
-    error(msg: string): void {
-        super.error("aero SW", msg);
-    }
-    fatalErr(msg: string): /* Response */ Error {
-        super.fatalErr("aero SW", msg);
-        return new Error(`Caught Fatal Error: ${msg}`);
-        /*
-        TODO: Only show the crash string when the proxy is in DEBUG mode
-        TODO: Unify the crash screen on the SW handler with extras and this one
-        return new Response(
-            /*
-            // TODO: Fix
-            this.options && "htmlTemplatingCallback" in this.options
-                ? `Fatal error:	 ${msg}`
-                : this.options.htmlTemplatingCallback(msg),
-                *\/
-            msg,
-            {
-                status: 500,
-                headers: {
-                    "content-type": "text/html"
-                }
-            }
-        );
-        */
-    }
+	log(msgs: string[] | string): void {
+		super.log("aero SW", msgs);
+	}
+	warn(msgs: string[] | string[] | string): void {
+		super.warn("aero SW", msgs);
+	}
+	debug(msgs: string[] | string): void {
+		super.warn("aero SW", msgs);
+	}
+	error(msgs: string[] | string): void {
+		super.error("aero SW", msgs);
+	}
+	fatalErr(msgs: string[] | string): /* Response */ Error {
+		super.fatalErr("aero SW", msgs);
+		return new Error(`Caught Fatal Error: ${msgs}`);
+		/*
+		TODO: Only show the crash string when the proxy is in DEBUG mode
+		TODO: Unify the crash screen on the SW handler with extras and this one
+		return new Response(
+			/*
+			// TODO: Fix
+			this.options && "htmlTemplatingCallback" in this.options
+				? `Fatal error:	 ${msgs}`
+				: this.options.htmlTemplatingCallback(msgs),
+				*\/
+			msg,
+			{
+				status: 500,
+				headers: {
+					"content-type": "text/html"
+				}
+			}
+		);
+		*/
+	}
 }
 
 // TODO: Support the seconary bubbling
 class AeroSandboxLogger extends GenericLogger {
-    options: LoggerOptions;
+	options: LoggerOptions;
 
-    constructor(options: LoggerOptions) {
-        super();
-        this.options = options;
-    }
+	constructor(options: LoggerOptions) {
+		super();
+		this.options = options;
+	}
 
-    log(msg: string): void {
-        super.log("aero sandbox", msg);
-    }
-    warn(msg: string): void {
-        super.warn("aero sandbox", msg);
-    }
-    error(msg: string): void {
-        super.error("aero sandbox", msg);
-    }
-    fatalErr(msg: string): void {
-        // TODO: INSTEAD MAKE THIS A CRASH SCREEN IF THE DEBUG FLAG IS ENABLED
-        super.fatalErr("aero sandbox", msg);
+	log(msgs: string[] | string): void {
+		super.log("aero sandbox", msgs);
+	}
+	warn(msgs: string[] | string): void {
+		super.warn("aero sandbox", msgs);
+	}
+	error(msgs: string[] | string): void {
+		super.error("aero sandbox", msgs);
+	}
+	fatalErr(msgs: string[] | string): void {
+		// TODO: INSTEAD MAKE THIS A CRASH SCREEN IF THE DEBUG FLAG IS ENABLED
+		super.fatalErr("aero sandbox", msgs);
 
-        if (this.options.htmlTemplatingCallback !== undefined)
-            this.options.htmlTemplatingCallback(msg);
-    }
+		if (this.options.htmlTemplatingCallback !== undefined)
+			this.options.htmlTemplatingCallback(msgs);
+	}
 }
 
 export { AeroLogger, AeroSandboxLogger };
 
 /** For the log bubbles */
 export default function genBubbleStyle(color: string): string {
-    return /* css */ `
+	return /* css */ `
 color: white;
 background-color: ${color};
 padding: 3px 6px 3px 6px;
