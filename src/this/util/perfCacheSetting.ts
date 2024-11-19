fb402e84b8f2540b3a3f04e1f75b0197c57e1516
Ryan Wilson <inbox@ryanwilson.space>
2024-11-19 02:09:57 -0500

    Skip caching when the Vary header is missing

--- src/this/util/perfCacheSetting.ts
+++ src/this/util/perfCacheSetting.ts
@@ -4,6 +4,7 @@ import { okAsync } from "neverthrow";
 
 // Passthrough type
 import type CacheManager from "../isolation/CacheManager";
+import { fmtNeverthrowErr } from "$aero/src/AeroSandbox/tests/shared/fmtErrTest";
 
 export default async function perfCacheSetting({
 	cacheMan,
@@ -12,9 +13,14 @@ export default async function perfCacheSetting({
 }: {
 	cacheMan: CacheManager, reqUrlHref: string, rewrittenResp: Response
 }): Promise<ResultAsync<void, Error>> {
+	const varyHeader = rewrittenResp.headers.get("vary");
+	if (varyHeader === null)
+		// Skip (we don't need to cache this)
+		return okAsync(undefined);
 	// Cache the response
-	const cacheManSetRes = await cacheMan.set(reqUrlHref, rewrittenResp, rewrittenResp.headers.get("vary"));
+	const cacheManSetRes = await cacheMan.set(reqUrlHref, rewrittenResp,);
 	if (cacheManSetRes.isErr())
 		return fmtNeverthrowErr("Failed to set the new emulated cache", cacheManSetRes.error.message);
+	logger.log("Cached the response: ", rewrittenResp);
 	return okAsync(undefined);
 }
